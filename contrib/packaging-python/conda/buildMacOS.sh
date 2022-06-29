cc85fd65c3cc16da16d91a4930aa918092805b69
Shlok Sabarwal <shlok190303@gmail.com>
2022-06-28 20:35:07 -0700

    Adding MacOS deployment for M1 chipset #2

--- contrib/packaging-python/conda/buildMacOS.sh
+++ contrib/packaging-python/conda/buildMacOS.sh
@@ -29,6 +29,11 @@ export LDFLAGS="-Wl,-undefined,dynamic_lookup $LDFLAGS"
 export MKL_INCLUDE_DIR=`cd $HOME/miniconda/pkgs/mkl-include-*/; pwd`
 export MKL_LIB_DIR=`cd $HOME/miniconda/pkgs/mkl-2022*/; pwd`
 CONFIGURATION=Release
+
+export PARDISO_MKL_ENABLE="OFF"
+if [`uname -m` == x86_64]; then
+    PARDISO_MKL_ENABLE="ON"
+
 # Configure step
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
  -DCMAKE_C_COMPILER=$(which clang) \
@@ -51,7 +56,7 @@ cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
  -DENABLE_MODULE_CASCADE=OFF \
  -DCASCADE_INCLUDE_DIR=$HOME/miniconda/include/opencascade \
  -DCASCADE_LIBDIR=$HOME/miniconda/lib \
- -DENABLE_MODULE_PARDISO_MKL=ON \
+ -DENABLE_MODULE_PARDISO_MKL=$(PARDISO_MKL_ENABLE) \
  -DMKL_INCLUDE_DIR=$MKL_INCLUDE_DIR/include \
  -DMKL_RT_LIBRARY=$MKL_LIB_DIR/lib/libmkl_rt.dylib \
  -DEIGEN3_INCLUDE_DIR="/usr/local/include/eigen3" "$HOME/miniconda/include" \
